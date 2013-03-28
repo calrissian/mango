d02e62fe4cc6cd49415d929e0e9491564937f3f6
Corey Nolet <cnolet@texeltek.com>
2013-03-28 11:35:17 -0400

    Adding distributed stream opener stuff

---
+++ uri/src/main/java/mango/uri/support/DataResolverConstants.java
@@ -0,0 +1,11 @@
+package mango.uri.support;
+
+import java.lang.String;
+
+public class DataResolverConstants {
+
+
+    public static final String SYSTEM_ALL = "any";
+
+    public static final String DELIM = "\u0000";
+}
