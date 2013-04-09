ff740e6f483ea119e61bbf277cf1e794ea75067b
Corey Nolet <cnolet@texeltek.com>
2013-04-09 08:11:41 -0400

    Adding merkle tree

---
+++ hash/src/main/java/org/calrissian/mango/hash/tree/HashLeaf.java
@@ -0,0 +1,53 @@
+package org.calrissian.mango.hash.tree;
+
+
+import java.util.List;
+
+/**
+ * A leaf represents a single hashed "bucket" of data- it needs to be sortable. Raw data should NOT be carried along
+ * as serializable properties of the Leaf because this will negate the compressed effect of the data structure.
+ */
+public abstract class HashLeaf implements Node, Comparable {
+
+    protected String hash;
+
+    public HashLeaf() {}
+
+    public HashLeaf(String hash) {
+        this.hash = hash;
+    }
+
+    @Override
+    public List<Node> getChildren() {
+        return null;
+    }
+
+    @Override
+    public String getHash() {
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() +  "{" +
+                "hash='" + hash + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof HashLeaf)) return false;
+
+        HashLeaf leaf = (HashLeaf) o;
+
+        if (hash != null ? !hash.equals(leaf.hash) : leaf.hash != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return hash != null ? hash.hashCode() : 0;
+    }
+}
---
+++ hash/src/main/java/org/calrissian/mango/hash/tree/Node.java
@@ -0,0 +1,23 @@
+package org.calrissian.mango.hash.tree;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * A node in a merkle tree representing a hash
+ */
+public interface Node extends Serializable {
+
+    /**
+     * Accessor for the children that this Node's hash is comprised of
+     * @return
+     */
+    List<Node> getChildren();
+
+    /**
+     * If this node has children, aggregates the hashes of the children. If this node is a leaf, represents the hash
+     * of the data.
+     * @return
+     */
+    String getHash();
+}
