710ae97a256170c3ce29a5a0452af098f710066d
Corey J. Nolet <cjnolet@gmail.com>
2014-05-09 21:49:03 -0400

    Adding license headers

--- mango-core/src/main/java/org/calrissian/mango/criteria/domain/criteria/AbstractKeyValueLeafCriteria.java
+++ mango-core/src/main/java/org/calrissian/mango/criteria/domain/criteria/AbstractKeyValueLeafCriteria.java
@@ -1,8 +1,20 @@
+/*
+ * Copyright (C) 2013 The Calrissian Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.calrissian.mango.criteria.domain.criteria;
 
-/**
- * Created by cjnolet on 5/9/14.
- */
 public abstract class AbstractKeyValueLeafCriteria extends LeafCriteria {
 
   protected String key;
