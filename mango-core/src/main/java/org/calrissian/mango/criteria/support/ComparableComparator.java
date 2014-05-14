c3d7d545bf22e43a97d8812dcc050662e4cdd4d8
Corey J. Nolet <cjnolet@gmail.com>
2014-05-13 21:05:22 -0400

    Making comparable comparatorimplement serializable

--- mango-core/src/main/java/org/calrissian/mango/criteria/support/ComparableComparator.java
+++ mango-core/src/main/java/org/calrissian/mango/criteria/support/ComparableComparator.java
@@ -1,8 +1,9 @@
 package org.calrissian.mango.criteria.support;
 
+import java.io.Serializable;
 import java.util.Comparator;
 
-public class ComparableComparator implements Comparator<Comparable> {
+public class ComparableComparator implements Comparator<Comparable>, Serializable {
   @Override
   public int compare(Comparable comparable, Comparable comparable2) {
     return comparable.compareTo(comparable2);
