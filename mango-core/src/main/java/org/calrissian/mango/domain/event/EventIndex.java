647806ca082a7f8ea51c6d028a48a1924b9381d4
Corey J. Nolet <cjnolet@gmail.com>
2014-05-27 00:28:42 -0400

    Adding hashcode and toString() to EventIndex

--- mango-core/src/main/java/org/calrissian/mango/domain/event/EventIndex.java
+++ mango-core/src/main/java/org/calrissian/mango/domain/event/EventIndex.java
@@ -19,4 +19,32 @@ public class EventIndex implements TemporalIdentifiable {
     public Long getTimestamp() {
         return timestamp;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        EventIndex that = (EventIndex) o;
+
+        if (timestamp != that.timestamp) return false;
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (int) (timestamp ^ (timestamp >>> 32));
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "EventIndex{" +
+                "id='" + id + '\'' +
+                ", timestamp=" + timestamp +
+                '}';
+    }
 }
