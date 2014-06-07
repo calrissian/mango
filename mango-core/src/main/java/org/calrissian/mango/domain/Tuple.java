206bf9216d603fa8dc8713f79fdecb9abcd41d90
Corey J. Nolet <cjnolet@gmail.com>
2014-06-06 21:14:00 -0400

    Adding metadata to tuple object

--- mango-core/src/main/java/org/calrissian/mango/domain/Tuple.java
+++ mango-core/src/main/java/org/calrissian/mango/domain/Tuple.java
@@ -16,15 +16,29 @@
 package org.calrissian.mango.domain;
 
 
+import com.google.common.collect.Maps;
+
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+/**
+ * A value class representing a key/value pair with a visiblity. This class is immutable.
+ */
 public class Tuple<T> implements Serializable {
 
     protected final String key;
     protected final T value;
     protected final String visibility;
+
+    /**
+     * Metadata allows the tuple to be extensible so that different services can read different properties without
+     * the need for inheritance.
+     */
+    protected final Map<String,Object> metadata = new HashMap<String, Object>();
 
     public Tuple(String key, T value, String visibility) {
         checkNotNull(key);
@@ -51,14 +65,40 @@ public class Tuple<T> implements Serializable {
         return visibility;
     }
 
+    /**
+     * Sets a key/value pair on the metadata for the current tuple.
+     */
+    public void setMetadataValue(String key, Object value) {
+        metadata.put(key, value);
+    }
+
+    /**
+     * Gets a key/value pair from the metadata for the current tuple.
+     */
+    public void getMetadataValue(String key) {
+        metadata.get(key);
+    }
+
+    public Set<String> metadataKeys() {
+        return metadata.keySet();
+    }
+
+    /**
+     * Gets a copy of the current metadata
+     */
+    public Map<String,Object> getMetadata() {
+        return Maps.newHashMap(metadata);
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Tuple)) return false;
+        if (o == null || getClass() != o.getClass()) return false;
 
         Tuple tuple = (Tuple) o;
 
         if (key != null ? !key.equals(tuple.key) : tuple.key != null) return false;
+        if (metadata != null ? !metadata.equals(tuple.metadata) : tuple.metadata != null) return false;
         if (value != null ? !value.equals(tuple.value) : tuple.value != null) return false;
         if (visibility != null ? !visibility.equals(tuple.visibility) : tuple.visibility != null) return false;
 
@@ -70,6 +110,7 @@ public class Tuple<T> implements Serializable {
         int result = key != null ? key.hashCode() : 0;
         result = 31 * result + (value != null ? value.hashCode() : 0);
         result = 31 * result + (visibility != null ? visibility.hashCode() : 0);
+        result = 31 * result + (metadata != null ? metadata.hashCode() : 0);
         return result;
     }
 
@@ -78,8 +119,8 @@ public class Tuple<T> implements Serializable {
         return "Tuple{" +
                 "key='" + key + '\'' +
                 ", value=" + value +
-                ", type=" + value.getClass() +
                 ", visibility='" + visibility + '\'' +
+                ", metadata=" + metadata +
                 '}';
     }
 }
