9611d5882cc73b2e7f4bd7840dc1822da5826431
Corey J. Nolet <cjnolet@gmail.com>
2014-07-04 09:09:05 -0400

    Putting entity deserialization test in the propert place

--- mango-json/src/test/java/org/calrissian/mango/json/tuplestore/EntityDeserializerTest.java
+++ mango-json/src/test/java/org/calrissian/mango/json/tuplestore/EntityDeserializerTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.calrissian.mango.json;
+package org.calrissian.mango.json.tuplestore;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
