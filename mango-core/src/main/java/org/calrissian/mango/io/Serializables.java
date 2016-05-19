450ab7113d74aab7d937136fa79b0319392d6f00
eawagner <ceraw79@hotmail.com>
2016-05-18 23:59:58 -0400

    Added comments to Serializable class.

--- mango-core/src/main/java/org/calrissian/mango/io/Serializables.java
+++ mango-core/src/main/java/org/calrissian/mango/io/Serializables.java
@@ -23,14 +23,23 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import static org.apache.commons.codec.binary.Base64.decodeBase64;
 import static org.apache.commons.codec.binary.Base64.encodeBase64;
 
+/**
+ * Utilities for converting serializable objects to byte arrays.
+ */
 public class Serializables {
-
     private Serializables() {}
 
+    /**
+     * Utility for turning a serializable object into a byte array.
+     */
     public static byte[] serialize(Serializable serializable) throws IOException {
         return serialize(serializable, false);
     }
 
+    /**
+     * Utility for turning a serializable object into a byte array. If the {@code compress} option is selected
+     * then the bytes will be run through gzip compression.
+     */
     public static byte [] serialize(Serializable serializable, boolean compress) throws IOException {
         checkNotNull(serializable);
 
@@ -43,10 +52,17 @@ public class Serializables {
         }
     }
 
+    /**
+     * Utility for returning a Serializable object from a byte array.
+     */
     public static <T extends Serializable> T deserialize(byte[] bytes) throws IOException, ClassNotFoundException {
         return deserialize(bytes, false);
     }
 
+    /**
+     * Utility for returning a Serializable object from a byte array. Only use the {@code compressed} option if the
+     * data was originally compresses when using the {@link Serializables#serialize(Serializable, boolean)} method.
+     */
     public static <T extends Serializable> T deserialize(byte[] bytes, boolean compressed) throws IOException, ClassNotFoundException {
         checkNotNull(bytes);
 
@@ -56,10 +72,17 @@ public class Serializables {
         }
     }
 
+    /**
+     * Utility for returning a Serializable object from a base 64 encoded byte array.
+     */
     public static byte[] toBase64(Serializable serializable) throws IOException {
         return encodeBase64(serialize(serializable));
     }
 
+    /**
+     * Utility for returning a Serializable object from a byte array. Only use this method if the data was originally
+     * serialized using the {@link Serializables#toBase64(Serializable)} method.
+     */
     public static <T extends Serializable>T fromBase64(byte[] bytes) throws IOException, ClassNotFoundException {
         return deserialize(decodeBase64(bytes));
     }
