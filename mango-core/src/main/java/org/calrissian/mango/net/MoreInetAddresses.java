2dcd320217d46b11f4b45dabf592737638b2a9ae
eawagner <ceraw79@hotmail.com>
2018-11-05 19:16:25 -0500

    Cleaned some minor cleanup.

--- mango-core/src/main/java/org/calrissian/mango/net/MoreInetAddresses.java
+++ mango-core/src/main/java/org/calrissian/mango/net/MoreInetAddresses.java
@@ -182,9 +182,9 @@ public class MoreInetAddresses {
      * @throws IllegalArgumentException if the argument is not a valid IPv4 mapped address
      */
     public static Inet4Address getMappedIPv4Address(Inet6Address ip) {
-        if (!isMappedIPv4Address(ip)) {
+        if (!isMappedIPv4Address(ip))
             throw new IllegalArgumentException(String.format("Address '%s' is not IPv4-mapped.", toAddrString(ip)));
-        }
+
         return getInet4Address(copyOfRange(ip.getAddress(), 12, 16));
     }
 
